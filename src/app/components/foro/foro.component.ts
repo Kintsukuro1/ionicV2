7baf79bed02c7dec1fada7d5ba332b8570e82152
Kintsukuro1 <127626152+Kintsukuro1@users.noreply.github.com>
2023-10-30 05:51:02 -0300

    Add files via upload

--- src/app/components/foro/foro.component.ts
+++ src/app/components/foro/foro.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-foro',
   templateUrl: './foro.component.html',
   styleUrls: ['./foro.component.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule,]
 })
 export class ForoComponent  implements OnInit {
 
--- src/app/components/misdatos/misdatos.component.ts
+++ src/app/components/misdatos/misdatos.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-misdatos',
   templateUrl: './misdatos.component.html',
   styleUrls: ['./misdatos.component.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule,]
 })
 export class MisdatosComponent  implements OnInit {
 
